39eb0910087f3dae618b3f2544f1144bac9495d0
Romain <romain.cheng@kciope.com>
2023-03-24 09:28:48 +0100

    remove useless import

--- app/src/main/java/com/example/k_fit/MainActivity.kt
+++ app/src/main/java/com/example/k_fit/MainActivity.kt
@@ -1,22 +1,10 @@
 package com.example.k_fit
 
 import android.os.Bundle
-import android.widget.StackView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -26,7 +14,6 @@ import com.example.k_fit.presentation.features.loginOrRegister.LoginOrRegisterSc
 import com.example.k_fit.presentation.features.register.RegisterScreen
 import com.google.firebase.FirebaseApp
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.*
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
